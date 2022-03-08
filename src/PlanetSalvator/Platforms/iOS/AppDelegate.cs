d5bb0cd3f2506232e900efc972391b57b27e2f8d
Alexis C. GRIDEL <alexis.gridel@pm.me>
2022-03-08 23:20:07 +0100

    feat: migrate from maui to blazor webassembly

--- src/PlanetSalvator/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace PlanetSalvator;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
