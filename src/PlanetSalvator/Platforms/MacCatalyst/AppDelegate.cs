2021c3a9dc7c28fb7bcf249e9386c629159825bf
Alexis C. GRIDEL <alexis.gridel@pm.me>
2022-03-08 15:13:04 +0100

    feat: add net maui project

---
+++ src/PlanetSalvator/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace PlanetSalvator;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
