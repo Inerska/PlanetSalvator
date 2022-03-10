a96bb45511fdd621ba3da7a71949b9cb5bdcc1e9
Alexis C. GRIDEL <alexis.gridel@pm.me>
2022-03-10 17:50:25 +0100

    feat: apply validation to authentication login page

---
+++ src/PlanetSalvator/Models/UserLogin.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Alexis Chân Gridel. All Rights Reserved.
+// Licensed under the GNU General Public License v3.0.
+// See the LICENSE file in the project root for more information.
+
+using System.ComponentModel.DataAnnotations;
+
+namespace PlanetSalvator.Models;
+
+public class UserLogin
+{
+    [Required] [EmailAddress] public string? EmailAddress { get; set; }
+
+    [Required]
+    [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.",
+        MinimumLength = 6)]
+    public string? Password { get; set; }
+}
---
+++ src/PlanetSalvator/Pages/Authentification/Login.razor.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Alexis Chân Gridel. All Rights Reserved.
+// Licensed under the GNU General Public License v3.0.
+// See the LICENSE file in the project root for more information.
+
+using PlanetSalvator.Models;
+
+namespace PlanetSalvator.Pages.Authentification;
+
+public partial class Login
+{
+    private readonly UserLogin _userLoginModel = new();
+}
