3ebb58bf5f8438feb9450f5fa1c31382a968d0c6
Alexis C. GRIDEL <alexis.gridel@pm.me>
2022-03-19 18:23:21 +0100

    feat: create ef default db context

---
+++ src/PlanetSalvator.Infrastructure.Persistence/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Alexis Chân Gridel. All Rights Reserved.
+// Licensed under the GNU General Public License v3.0.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.EntityFrameworkCore;
+
+namespace PlanetSalvator.Infrastructure.Persistence.Contexts;
+
+public class ApplicationDbContext 
+    : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    { }
+}
