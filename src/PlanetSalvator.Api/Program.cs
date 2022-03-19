f693a3635b19699ddbfb8ca604990fd95ddc3308
Alexis C. GRIDEL <alexis.gridel@pm.me>
2022-03-19 18:58:24 +0100

    feat: resolve authentication and oidc configuration

--- src/PlanetSalvator.Api/Program.cs
+++ src/PlanetSalvator.Api/Program.cs
@@ -18,29 +18,6 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddAuthentication()
-    .AddOpenIdConnect(options =>
-    {
-        options.ClientId = "PlanetSalvator.Api";
-        options.Authority = "https://localhost:44300/";
-        options.RequireHttpsMetadata = false;
-        options.ResponseType = OpenIdConnectResponseType.Code;
-        options.GetClaimsFromUserInfoEndpoint = true;
-        options.SaveTokens = true;
-        options.Scope.Add("openid");
-        options.Scope.Add("profile");
-        options.Scope.Add("email");
-        options.Scope.Add("roles");
-        options.Events.OnRedirectToIdentityProvider = async context =>
-        {
-            context.ProtocolMessage.SetParameter("audience", "https://localhost:44300/");
-        };
-        options.Events.OnRemoteFailure = async context =>
-        {
-            context.HandleResponse();
-            context.Response.Redirect("/");
-        };
-    });
 builder.Services.AddDbContext<ApplicationDbContext>(optionsBuilder =>
 {
     var connectionString = Environment.GetEnvironmentVariable("DefaultConnectionString");
@@ -55,6 +32,46 @@ builder.Services.Configure<IdentityOptions>(options =>
     options.ClaimsIdentity.UserIdClaimType = OpenIddictConstants.Claims.Name;
     options.ClaimsIdentity.UserIdClaimType = OpenIddictConstants.Claims.Subject;
     options.ClaimsIdentity.RoleClaimType = OpenIddictConstants.Claims.Role;
+});
+
+builder.Services.AddOpenIddict()
+    .AddCore(coreBuilder => coreBuilder.UseEntityFrameworkCore().UseDbContext<ApplicationDbContext>())
+    .AddServer(serverBuilder =>
+    {
+        serverBuilder.SetTokenEndpointUris("/connect/token");
+        serverBuilder.SetUserinfoEndpointUris("/connect/userinfo");
+
+        serverBuilder.AllowPasswordFlow();
+        serverBuilder.AllowRefreshTokenFlow();
+
+        serverBuilder.AllowCustomFlow("custom_flow_name");
+
+        serverBuilder.UseReferenceAccessTokens();
+        serverBuilder.UseReferenceRefreshTokens();
+
+        serverBuilder.RegisterScopes(
+            OpenIddictConstants.Permissions.Scopes.Email,
+            OpenIddictConstants.Permissions.Scopes.Profile,
+            OpenIddictConstants.Permissions.Scopes.Roles);
+
+        serverBuilder.SetAccessTokenLifetime(TimeSpan.FromMinutes(30));
+        serverBuilder.SetRefreshTokenLifetime(TimeSpan.FromDays(7));
+
+        serverBuilder.AddDevelopmentEncryptionCertificate()
+            .AddDevelopmentSigningCertificate();
+
+        serverBuilder.UseAspNetCore().EnableTokenEndpointPassthrough();
+    })
+    .AddValidation(validationBuilder =>
+    {
+        validationBuilder.UseLocalServer();
+        validationBuilder.UseAspNetCore();
+    });
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = OpenIddictConstants.Schemes.Bearer;
+    options.DefaultChallengeScheme = OpenIddictConstants.Schemes.Bearer;
 });
 
 builder.Services.AddScoped<IDataFetcherService, ClimateChangeNewsFetcherService>();
