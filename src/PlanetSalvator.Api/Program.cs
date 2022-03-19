f6218fc2e26bba063fc3fa414984dce0d49e3b6f
Alexis C. GRIDEL <alexis.gridel@pm.me>
2022-03-19 18:50:38 +0100

    feat: configure oidc authentication w/ efcore configuration

--- src/PlanetSalvator.Api/Program.cs
+++ src/PlanetSalvator.Api/Program.cs
@@ -2,7 +2,11 @@
 // Licensed under the GNU General Public License v3.0.
 // See the LICENSE file in the project root for more information.
 
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 using PlanetSalvator.BusinessLayer.Services;
+using PlanetSalvator.Infrastructure.Persistence.Contexts;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,8 +16,40 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAuthentication()
+    .AddOpenIdConnect(options =>
+    {
+        options.ClientId = "PlanetSalvator.Api";
+        options.Authority = "https://localhost:44300/";
+        options.RequireHttpsMetadata = false;
+        options.ResponseType = OpenIdConnectResponseType.Code;
+        options.GetClaimsFromUserInfoEndpoint = true;
+        options.SaveTokens = true;
+        options.Scope.Add("openid");
+        options.Scope.Add("profile");
+        options.Scope.Add("email");
+        options.Scope.Add("roles");
+        options.Events.OnRedirectToIdentityProvider = async context =>
+        {
+            context.ProtocolMessage.SetParameter("audience", "https://localhost:44300/");
+        };
+        options.Events.OnRemoteFailure = async context =>
+        {
+            context.HandleResponse();
+            context.Response.Redirect("/");
+        };
+    });
+builder.Services.AddDbContext<ApplicationDbContext>(optionsBuilder =>
+{
+    var connectionString = Environment.GetEnvironmentVariable("DefaultConnectionString");
+    ArgumentNullException.ThrowIfNull(connectionString, nameof(connectionString));    
+    
+    optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+    optionsBuilder.UseOpenIddict();
+});
 
-builder.Services.AddScoped<IDataFetcherService, ClimateChangeNewsFetcherService>(); 
+builder.Services.AddScoped<IDataFetcherService, ClimateChangeNewsFetcherService>();
+
 
 var app = builder.Build();
 
