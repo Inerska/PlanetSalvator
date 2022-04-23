39ca37c01431390421200730f5466923c259acbc
Alexis C. Gridel <alexis.gridel@pm.me>
2022-04-23 19:41:06 +0200

    feat(identity-page): register override default page

--- src/PlanetSalvator.Web/Server/Areas/Identity/Pages/Account/LoginWith2fa.cshtml.cs
+++ src/PlanetSalvator.Web/Server/Areas/Identity/Pages/Account/LoginWith2fa.cshtml.cs
@@ -1,22 +1,18 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
-#nullable disable
-
-using System;
-using System.ComponentModel.DataAnnotations;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
-using PlanetSalvator.Web.Server.Models;
-
 namespace PlanetSalvator.Web.Server.Areas.Identity.Pages.Account
 {
+#nullable disable
+
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+    using Microsoft.Extensions.Logging;
+    using PlanetSalvator.Web.Server.Models;
+
     public class LoginWith2faModel : PageModel
     {
         private readonly SignInManager<ApplicationUser> _signInManager;
