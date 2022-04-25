9d0a38f554c7f0d88d6f9deb79b1d2169f8261f4
Alexis C. Gridel <alexis.gridel@pm.me>
2022-04-26 01:44:42 +0200

    fix: api get request method distinction with different route path

--- src/PlanetSalvator.Web/Server/Controllers/DailyPublicUserTaskController.cs
+++ src/PlanetSalvator.Web/Server/Controllers/DailyPublicUserTaskController.cs
@@ -2,11 +2,14 @@
 // Licensed under the GNU General Public License v3.0.
 // See the LICENSE file in the project root for more information.
 
+using PlanetSalvator.Web.Server.Handlers;
+using PlanetSalvator.Web.Shared;
+
 namespace PlanetSalvator.Web.Server.Controllers;
 
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using PlanetSalvator.Infrastructure.Extensions;
+using PlanetSalvator.Web.Server.Commands;
 using PlanetSalvator.Web.Server.Queries;
 
 [ApiController]
@@ -18,7 +21,7 @@ public class DailyPublicUserTaskController
     private readonly IMediator _mediator;
 
     /// <summary>
-    /// Initializes a new instance of the <see cref="DailyPublicUserTaskController"/> class.
+    ///     Initializes a new instance of the <see cref="DailyPublicUserTaskController" /> class.
     /// </summary>
     /// <param name="logger"></param>
     /// <param name="mediator"></param>
@@ -31,7 +34,7 @@ public class DailyPublicUserTaskController
     }
 
     /// <summary>
-    /// Simulate an extern api call to get the daily task.
+    ///     Simulate an extern api call to get the daily task.
     /// </summary>
     /// <param name="tasksCount">The count of daily tasks to retrieve.</param>
     /// <returns>The tasksCount daily tasks retrieved.</returns>
@@ -39,5 +42,47 @@ public class DailyPublicUserTaskController
     public async Task<IActionResult> GetDailyPublicUserTask([FromQuery] int tasksCount = 3)
     {
         return Ok(await _mediator.Send(new GetDailyTasksQuery(tasksCount)));
+    }
+
+    [HttpGet]
+    [Route("/api/[controller]/byEmail")]
+    public async Task<IActionResult> GetDailyPublicUserTaskByEmail([FromQuery] string email)
+    {
+        return Ok(await _mediator.Send(new GetDailyTasksByEmailQuery(email)));
+    }
+
+    /// <summary>
+    ///     Set the daily tasks for the current user.
+    /// </summary>
+    /// <param name="email">The user's email.</param>
+    /// <returns>A <see cref="Task{TResult}" /> representing the result of the asynchronous operation.</returns>
+    [Consumes("application/json")]
+    [HttpPost]
+    public async Task<IActionResult> SetDailyPublicUserTask([FromBody] Temp email)
+    {
+        // If the mediator return an exception then returns NotFound.
+        try
+        {
+            await _mediator.Send(new InsertDailyTasksCommandByEmail(email));
+        }
+        catch (KeyNotFoundException)
+        {
+            return NotFound();
+        }
+
+        return Ok();
+    }
+
+    /// <summary>
+    ///     Delete a daily task for the current user.
+    /// </summary>
+    /// <param name="email">The user's email.</param>
+    /// <param name="task">The task content.</param>
+    /// <returns>A <see cref="Task{TResult}"/> representing the result of the asynchronous operation.</returns>
+    [HttpDelete]
+    public async Task<IActionResult> DeleteDailyPublicUserTask([FromQuery] string email, Guid guid)
+    {
+        await _mediator.Send(new DeleteDailyTaskCommandByGuid(email, guid));
+        return Ok();
     }
 }
