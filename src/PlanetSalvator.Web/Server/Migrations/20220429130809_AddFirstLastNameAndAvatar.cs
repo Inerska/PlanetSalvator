26a601b6c98b524cd9dbc6c744fb032041962506
Alexis C. Gridel <alexis.gridel@pm.me>
2022-04-29 15:09:59 +0200

    feat(dal): add first name/ last name/ avatar byte

---
+++ src/PlanetSalvator.Web/Server/Migrations/20220429130809_AddFirstLastNameAndAvatar.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PlanetSalvator.Web.Server.Migrations
+{
+    public partial class AddFirstLastNameAndAvatar : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "AspNetUsers",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "AspNetUsers",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "AspNetUsers");
+        }
+    }
+}
--- src/PlanetSalvator.Web/Shared/ApplicationUser.cs
+++ src/PlanetSalvator.Web/Shared/ApplicationUser.cs
@@ -1,10 +1,20 @@
-﻿namespace PlanetSalvator.Web.Shared;
+﻿// Copyright (c) Alexis Chân Gridel. All Rights Reserved.
+// Licensed under the GNU General Public License v3.0.
+// See the LICENSE file in the project root for more information.
+
+namespace PlanetSalvator.Web.Shared;
 
 using Microsoft.AspNetCore.Identity;
 
 public class ApplicationUser
     : IdentityUser
 {
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public byte[] Avatar { get; set; }
+
     public int Points { get; set; }
 
     public double Latitude { get; set; }
