bcacf587411a9965577f0c682c9238b7c0b79d01
Alexis C. Gridel <alexis.gridel@pm.me>
2022-04-29 14:40:08 +0200

    feat(migrations): add quiz

---
+++ src/PlanetSalvator.Web/Server/Migrations/20220429123654_AddQuizFeat.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PlanetSalvator.Web.Server.Migrations
+{
+    public partial class AddQuizFeat : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Quizzes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Author = table.Column<string>(type: "TEXT", nullable: false),
+                    Points = table.Column<int>(type: "INTEGER", nullable: false),
+                    Question = table.Column<string>(type: "TEXT", nullable: false),
+                    AnswerA = table.Column<string>(type: "TEXT", nullable: false),
+                    AnswerB = table.Column<string>(type: "TEXT", nullable: false),
+                    AnswerC = table.Column<string>(type: "TEXT", nullable: false),
+                    AnswerD = table.Column<string>(type: "TEXT", nullable: false),
+                    CorrectAnswer = table.Column<string>(type: "TEXT", nullable: false),
+                    Explanation = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Quizzes", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Quizzes");
+        }
+    }
+}
