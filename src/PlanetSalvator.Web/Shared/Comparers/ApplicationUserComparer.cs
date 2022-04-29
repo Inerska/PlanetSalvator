7995d31716ae4d85852085ffae3d57e3161ce573
Alexis C. Gridel <alexis.gridel@pm.me>
2022-04-29 15:41:23 +0200

    feat: application user comparer by points

---
+++ src/PlanetSalvator.Web/Shared/Comparers/ApplicationUserComparer.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Alexis Chân Gridel. All Rights Reserved.
+// Licensed under the GNU General Public License v3.0.
+// See the LICENSE file in the project root for more information.
+
+namespace PlanetSalvator.Web.Shared.Comparers;
+
+public class ApplicationUserComparer
+    : IComparer<ApplicationUser>
+{
+    /// <inheritdoc/>
+    public int Compare(ApplicationUser x, ApplicationUser y)
+    {
+        return x.Points > y.Points ? -1 : x.Points < y.Points ? 1 : 0;
+    }
+}
