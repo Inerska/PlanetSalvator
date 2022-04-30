3257c8cd416e11caf573b8a337fc012e9cc2a874
Alexis C. Gridel <alexis.gridel@pm.me>
2022-04-30 09:55:34 +0200

    feat(tests): add test for sort method extension

--- src/PlanetSalvator.Infrastructure.Test/Extensions/EnumerableExtensionsTests.cs
+++ src/PlanetSalvator.Infrastructure.Test/Extensions/EnumerableExtensionsTests.cs
@@ -2,7 +2,9 @@
 // Licensed under the GNU General Public License v3.0.
 // See the LICENSE file in the project root for more information.
 
+using System.Collections;
 using System.Linq;
+using PlanetSalvator.Web.Shared.Comparers;
 
 namespace PlanetSalvator.Infrastructure.Test.Extensions;
 
@@ -38,5 +40,14 @@ static class EnumerableExtensionsTests
 
         shuffled.ShouldNotBe(enumerable);
         shuffled.ElementAt(0).ShouldNotBe(enumerable.ElementAt(0));
+    }
+
+    [Test]
+    public static void Enumerable_Should_Be_Getting_Sorted_When_Using_Sort_Extension()
+    {
+        var enumerable = new List<int> { 5, 4, 1, 2, 3 };
+        var sorted = enumerable.Sort<int>(new IntComparer());
+
+        sorted.ShouldBe(new List<int> { 1, 2, 3, 4, 5 });
     }
 }
---
+++ src/PlanetSalvator.Web/Shared/Comparers/IntComparer.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Alexis Chân Gridel. All Rights Reserved.
+// Licensed under the GNU General Public License v3.0.
+// See the LICENSE file in the project root for more information.
+
+namespace PlanetSalvator.Web.Shared.Comparers;
+
+public class IntComparer
+    : IComparer<int>
+{
+    public int Compare(int x, int y)
+    {
+        return x.CompareTo(y);
+    }
+}
