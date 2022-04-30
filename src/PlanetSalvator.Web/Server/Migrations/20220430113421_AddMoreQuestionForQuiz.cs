863ecefc7b2d0ee1489f4bc6ab21f1e9d64e80bc
Alexis C. Gridel <alexis.gridel@pm.me>
2022-04-30 13:34:55 +0200

    feat(dal): add more questions to quiz seeder

---
+++ src/PlanetSalvator.Web/Server/Migrations/20220430113421_AddMoreQuestionForQuiz.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PlanetSalvator.Web.Server.Migrations
+{
+    public partial class AddMoreQuestionForQuiz : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Quizzes",
+                keyColumn: "Id",
+                keyValue: -1,
+                column: "Points",
+                value: 47);
+
+            migrationBuilder.InsertData(
+                table: "Quizzes",
+                columns: new[] { "Id", "AnswerA", "AnswerB", "AnswerC", "AnswerD", "Author", "CorrectAnswer", "Explanation", "Points", "Question" },
+                values: new object[] { -7, "500kg / personne", "700kg/ déchets ménagers par personne", "150 kg / déchets ménagers par personne", "1 tonne / déchets ménagers par personne", "Alexis Chân Gridel", "500kg / personne", "Il génère 45% des émissions de CO2 liées à l'énergie", 3, "Sur une année, un français produit en moyenne :" });
+
+            migrationBuilder.InsertData(
+                table: "Quizzes",
+                columns: new[] { "Id", "AnswerA", "AnswerB", "AnswerC", "AnswerD", "Author", "CorrectAnswer", "Explanation", "Points", "Question" },
+                values: new object[] { -6, "Le nucléaire", "Le charbon", "Le gaz", null, "Alexis Chân Gridel", "Le gaz", "Il génère 45% des émissions de CO2 liées à l'énergie", 27, "La source d'énergie la plus néfaste est :" });
+
+            migrationBuilder.InsertData(
+                table: "Quizzes",
+                columns: new[] { "Id", "AnswerA", "AnswerB", "AnswerC", "AnswerD", "Author", "CorrectAnswer", "Explanation", "Points", "Question" },
+                values: new object[] { -5, "Le nucléaire", "Le charbon", "Le gaz", null, "Alexis Chân Gridel", "Le gaz", null, 51, "La source d'énergie la plus néfaste est :" });
+
+            migrationBuilder.InsertData(
+                table: "Quizzes",
+                columns: new[] { "Id", "AnswerA", "AnswerB", "AnswerC", "AnswerD", "Author", "CorrectAnswer", "Explanation", "Points", "Question" },
+                values: new object[] { -4, "1979", "1988", "2000", null, "Alexis Chân Gridel", "1979", null, 38, "La 1ère COP a eu lieu en :" });
+
+            migrationBuilder.InsertData(
+                table: "Quizzes",
+                columns: new[] { "Id", "AnswerA", "AnswerB", "AnswerC", "AnswerD", "Author", "CorrectAnswer", "Explanation", "Points", "Question" },
+                values: new object[] { -3, "L'océan", "La forêt", null, null, "Alexis Chân Gridel", "L'océan", null, 57, "Le principal \"poumon\" de la planète est :" });
+
+            migrationBuilder.InsertData(
+                table: "Quizzes",
+                columns: new[] { "Id", "AnswerA", "AnswerB", "AnswerC", "AnswerD", "Author", "CorrectAnswer", "Explanation", "Points", "Question" },
+                values: new object[] { -2, "500 ans", "1 an", "1000 ans", null, "Alexis Chân Gridel", "1000 ans", null, 69, "Un sac plastique se décompose en :" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Quizzes",
+                keyColumn: "Id",
+                keyValue: -7);
+
+            migrationBuilder.DeleteData(
+                table: "Quizzes",
+                keyColumn: "Id",
+                keyValue: -6);
+
+            migrationBuilder.DeleteData(
+                table: "Quizzes",
+                keyColumn: "Id",
+                keyValue: -5);
+
+            migrationBuilder.DeleteData(
+                table: "Quizzes",
+                keyColumn: "Id",
+                keyValue: -4);
+
+            migrationBuilder.DeleteData(
+                table: "Quizzes",
+                keyColumn: "Id",
+                keyValue: -3);
+
+            migrationBuilder.DeleteData(
+                table: "Quizzes",
+                keyColumn: "Id",
+                keyValue: -2);
+
+            migrationBuilder.UpdateData(
+                table: "Quizzes",
+                keyColumn: "Id",
+                keyValue: -1,
+                column: "Points",
+                value: 79);
+        }
+    }
+}
--- src/PlanetSalvator.Web/Server/Migrations/Seeders/Initial/QuizSeeder.cs
+++ src/PlanetSalvator.Web/Server/Migrations/Seeders/Initial/QuizSeeder.cs
@@ -21,7 +21,68 @@ public static class QuizSeeder
                     CorrectAnswer = "Oui",
                     AnswerA = "Oui",
                     AnswerB = "Non",
-                }
-            );
+                },
+                new Quiz
+                {
+                    Id = -2,
+                    Question =
+                        "Un sac plastique se décompose en :",
+                    CorrectAnswer = "1000 ans",
+                    AnswerA = "500 ans",
+                    AnswerB = "1 an",
+                    AnswerC = "1000 ans",
+                },
+                new Quiz
+                {
+                    Id = -3,
+                    Question =
+                        "Le principal \"poumon\" de la planète est :",
+                    CorrectAnswer = "L'océan",
+                    AnswerA = "L'océan",
+                    AnswerB = "La forêt",
+                },
+                new Quiz
+                {
+                    Id = -4,
+                    Question =
+                        "La 1ère COP a eu lieu en :",
+                    CorrectAnswer = "1979",
+                    AnswerA = "1979",
+                    AnswerB = "1988",
+                    AnswerC = "2000",
+                },
+                new Quiz
+                {
+                    Id = -5,
+                    Question =
+                        "La source d'énergie la plus néfaste est :",
+                    CorrectAnswer = "Le gaz",
+                    AnswerA = "Le nucléaire",
+                    AnswerB = "Le charbon",
+                    AnswerC = "Le gaz",
+                },
+                new Quiz
+                {
+                    Id = -6,
+                    Question =
+                        "La source d'énergie la plus néfaste est :",
+                    CorrectAnswer = "Le gaz",
+                    AnswerA = "Le nucléaire",
+                    AnswerB = "Le charbon",
+                    AnswerC = "Le gaz",
+                    Explanation = "Il génère 45% des émissions de CO2 liées à l'énergie",
+                },
+                new Quiz
+                {
+                    Id = -7,
+                    Question =
+                        "Sur une année, un français produit en moyenne :",
+                    CorrectAnswer = "500kg / personne",
+                    AnswerA = "500kg / personne",
+                    AnswerB = "700kg/ déchets ménagers par personne",
+                    AnswerC = "150 kg / déchets ménagers par personne",
+                    AnswerD = "1 tonne / déchets ménagers par personne",
+                    Explanation = "Il génère 45% des émissions de CO2 liées à l'énergie",
+                });
     }
 }
